0e5a8b47566c8aff3427899e6ae320a83b7f14d6
Rishab Sanyal <rishabh106@gmail.com>
2026-04-14 14:07:04 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,43 @@
+"""
+Using a heap makes everything O(nlgn) -> because of heap insertion which is log(n)
+
+We could use a tracker number but updating that will be a hassle, right?
+
+OHHH we track the min number at EVERY POINT in the stack
+
+We insert the value AND the min number at that point, in the stack
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_num = None
+
+    def push(self, val: int) -> None:
+
+        if (self.min_num is None) or (self.min_num > val):
+            self.min_num = val
+
+        self.stack.append((val, self.min_num))
+
+    def pop(self) -> None:
+        result = self.stack.pop()[0]
+        if self.stack:
+            self.min_num = self.stack[-1][1]
+        else:
+            self.min_num = None
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+"""
+
+
+
+"""
+        
