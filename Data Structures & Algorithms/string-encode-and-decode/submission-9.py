8c7b8a3fbd56aa21a2446cfe17b8d168d355a8c8
Rishab Sanyal <rishabh106@gmail.com>
2026-04-13 22:43:01 -0700

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,7 @@
+class Solution:
+    DELIMITER = "-|-|-"
+    def encode(self, strs: List[str]) -> str:
+        return self.DELIMITER.join(strs) if strs else []
+
+    def decode(self, s: str) -> List[str]:
+        return s.split(self.DELIMITER)
