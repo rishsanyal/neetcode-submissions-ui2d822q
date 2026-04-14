afcd894e9aa728f4358a423fa4f5a81db7368bc3
Rishab Sanyal <rishabh106@gmail.com>
2026-04-14 15:36:05 -0700

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        """
+        use a stack here because we ONLY care about the asteroid on top
+        for every incoming asteroid, we compare it against the previous one
+
+        we pop if it explodes (bigger or eq asteroid comes in, in the other direction)
+        we insert if it goes in the same direction
+
+        asteroid_direction, asteroid
+        """
+
+        stack = []
+
+        for asteroid in asteroids:
+            asteroid_direction = "left" if asteroid < 0 else "right"
+            
+            if not stack or asteroid_direction == "right":
+                stack.append((asteroid_direction, abs(asteroid)))
+            else:
+                if asteroid_direction == "left" and stack[-1][0] == "right":
+                    # we compare
+                    if abs(asteroid) >= stack[-1][1]:
+                        stack.pop()
+
+
+
+
+        return [i[1] for i in stack]
