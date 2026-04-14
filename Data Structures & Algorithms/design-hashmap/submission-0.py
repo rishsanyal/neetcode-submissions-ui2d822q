9c52bad3e02653c2dd337a10f972c361a113854a
Rishab Sanyal <rishabh106@gmail.com>
2026-04-13 21:44:25 -0700

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.py
@@ -0,0 +1,45 @@
+"""
+We could have multiple elements per key? NO
+1 key - 1 element
+0 <= key, value <= 1,000,000
+
+We coudl actually implement a hashmap
+- An array with a hash function
+
+
+Hashing Collisions:
+- Open Addressing - Find the next free spot
+- Chaining - Use a linked list
+    - With chaining how do we know which key we're looking at? We iterate through the linkedlist node
+
+"""
+
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+
+class MyHashMap:
+
+    def __init__(self):
+        self.tracker = [None] * 1_000_000
+
+    def put(self, key: int, value: int) -> None:
+        self.tracker[key] = value
+
+    def get(self, key: int) -> int:
+        return self.tracker[key] or -1
+
+    def remove(self, key: int) -> None:
+        self.tracker[key] = None
+
+    def __hash_func(self, key):
+        return key % 1_000_000
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
