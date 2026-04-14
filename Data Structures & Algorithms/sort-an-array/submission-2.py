63dcab7623ed45d8175bffb5d3a66f9e992e2f33
Rishab Sanyal <rishabh106@gmail.com>
2026-04-14 08:45:53 -0700

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        """
+        Merge Sort
+        - Pick a pivot
+        - split the list around it
+        - merge it
+
+        can we do it in place?
+        """
+
+        def mergesort(inp):
+            pivot = (0 + len(inp)) // 2
+            lt, gt, eq = [], [], []
+
+            if not inp:
+                return []
+            
+            if len(inp) == 1:
+                return inp
+
+            for num in inp:
+                if num < inp[pivot]:
+                    lt.append(num)
+                elif num == inp[pivot]:
+                    eq.append(num)
+                else:
+                    gt.append(num)
+
+            return mergesort(lt) + eq + mergesort(gt)
+
+        return mergesort(nums)
+
+                
+                
+
