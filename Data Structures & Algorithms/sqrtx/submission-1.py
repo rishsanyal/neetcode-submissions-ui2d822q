754e344aabbfe8b671adf660be2e2b553bce75bc
Rishab Sanyal <rishabh106@gmail.com>
2026-04-20 10:27:59 -0700

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        """
+        - We know that any number between 0 - n is possible to be sqrt
+        - We can go to mid - get the square 
+            - if it's lower we increase l by mid + 1?
+            - elif -> mid = r - 1 or return
+        """
+
+        l, r = 0, x
+
+        while l <= r:
+            mid = (l+r)//2
+            mid_square = mid*mid
+
+            if mid_square == x:
+                return mid
+            elif mid_square < x:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return r
