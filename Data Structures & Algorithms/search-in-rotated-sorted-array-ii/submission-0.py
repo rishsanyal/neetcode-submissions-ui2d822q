b1950677239d083d26f5f87f14af7f884053244f
Rishab Sanyal <rishabh106@gmail.com>
2026-04-21 10:48:28 -0700

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        """
+        - We need binary search because the list is ordered already 
+
+        Assume ideal case and then everything else is covered under else.
+        """
+
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l+r)//2
+
+            if nums[mid] == target:
+                return True
+
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return False
