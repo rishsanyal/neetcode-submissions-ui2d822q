dede43166ea9715e772ae34022e80b2a854fbe17
Rishab Sanyal <rishabh106@gmail.com>
2026-04-21 10:10:45 -0700

    Add: capacity-to-ship-packages-within-d-days - submission-1

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-1.py
@@ -0,0 +1,51 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        """
+        2 parts to this problem
+            1. Search the least max_weight - N
+            2. Validate that max_weight sends the packages in N days
+
+            We want the least weight, in the number of days
+            We care more about weight than days
+        """
+
+        def validate_weight(max_weight):
+            itr = 0
+            days = 0
+            curr_weight = 0
+
+            while itr < len(weights):
+                if (curr_weight + weights[itr]) > max_weight:
+                    days += 1
+                    curr_weight = weights[itr]
+                elif (curr_weight + weights[itr]) < max_weight:
+                    curr_weight += weights[itr]
+                else:
+                    days += 1
+                    curr_weight = 0
+
+                itr += 1
+
+            if curr_weight > 0:
+                days += 1
+
+            return days
+
+        l, r = max(weights), max(weights)*days
+        ans = max(weights)
+
+        while l <= r:
+            mid_weight = (l+r)//2
+            possible = (validate_weight(mid_weight) <= days)
+            
+            if not possible:
+                # go right
+                l = mid_weight + 1
+                ans = min(ans, mid_weight)
+            else:
+                # go left
+                r = mid_weight - 1
+
+        print(l, r)
+        return l
+
