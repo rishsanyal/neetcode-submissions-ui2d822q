30d8063b98e88c456e41e0780d8735dc1a679791
Rishab Sanyal <rishabh106@gmail.com>
2026-04-22 14:00:35 -0700

    Add: balanced-binary-tree - submission-7

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        res = True
+
+        def traverse(node):
+            nonlocal res
+            if not node or res is False:
+                return 0
+
+            l = traverse(node.left)
+            r = traverse(node.right)
+
+            res = (abs(l-r) <= 1) and res
+
+            return 1 + max(l, r)
+
+        traverse(root)
+
+        return res
