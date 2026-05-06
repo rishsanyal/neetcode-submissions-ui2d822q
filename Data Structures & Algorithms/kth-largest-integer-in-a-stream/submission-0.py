e634b2bdb9bc69b86581f60716e26bd27031409d
Rishab Sanyal <rishabh106@gmail.com>
2026-05-06 11:29:55 -0600

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.py
@@ -0,0 +1,26 @@
+class KthLargest:
+    """
+    We maintain a heap of K elements
+    on every insertion we insert in heap and if it's length == K+1 -> we pop
+
+    but we want the Kth largest.
+    We need to maintain a min heap and get the max at every point?
+
+    do we just maintain a min heap of K points at every time?
+    """
+
+    def __init__(self, k: int, nums: List[int]):
+        import heapq
+        
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
+
+        while len(self.nums) >= k:
+            heapq.heappop(self.nums)
+        
+    def add(self, val: int) -> int:
+        return heapq.heappushpop(self.nums, val)
+
+
+        
