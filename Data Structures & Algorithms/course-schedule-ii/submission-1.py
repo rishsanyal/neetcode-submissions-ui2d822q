f9550b4dc1a141a64033316aaff60067a7656dff
Rishab Sanyal <rishabh106@gmail.com>
2026-05-12 10:47:55 -0600

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,64 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        """
+        DFS should still work
+        we just need to add it to a global list?
+
+        what's the numCourses for? We can still iterate, 
+            we just need to add results in the front of a list
+
+        we also have courses not in list, but those can go anywhere
+
+        make graph
+        res = []
+
+        start iterating through numCourses
+        if not in graph - add to res
+
+        if in graph: DFS
+        if cycle - return False, []
+        if true - return temp list and append to res
+        """
+
+        graph = defaultdict(set)
+        res = []
+
+        for a,b in prerequisites:
+            graph[a].add(b)
+
+        def foo(course, visited, temp_res):
+            nonlocal res
+
+            if course in visited:
+                return False
+
+            if course not in graph:
+
+                res.append(course)
+                return True
+
+            visited.add(course)
+
+            for req in graph[course]:
+                small_res = []
+                status = foo(req, visited, small_res)
+
+                if not status:
+                    return False
+
+            visited.remove(course)
+            graph.pop(course)
+
+            res.append(course)
+
+            return True
+
+
+        for i in range(numCourses):
+            status = foo(i, set(), [])
+
+            if not status:
+                return []
+
+
+        return res
