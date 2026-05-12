e7827959d2f8e81a4a98e2c39161b87512b60ca4
Rishab Sanyal <rishabh106@gmail.com>
2026-05-12 13:49:17 -0600

    Add: course-schedule-iv - submission-0

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-0.py
@@ -0,0 +1,58 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        """
+        We need a flat structure for easy search
+        - we can add all pre-reqs as we iterate OR
+            we can do a DFS and iterate until we hit the end or the course we're looking for
+        
+        What if there's a cycle?
+        - We need to detect and return False
+
+        We create a directed graph
+        - course -> prereq
+
+        We can iterate through numCourses and make it flat and check for cycles
+        If no cycles - add to the dict and keep going on
+        
+        iterate through queries in the end.
+        """
+
+        graph = defaultdict(set)
+        res = []
+        reqs = set()
+
+        for (prereq, course) in prerequisites:
+            graph[course].add(prereq)
+
+
+        def dfs(course, visited):
+            if course in visited:
+                return
+
+            visited.add(course)
+
+            for req_course in graph.get(course, []):
+                dfs(req_course, visited)
+
+            visited.remove(course)
+            reqs.add(course)
+
+            return
+
+
+        for i in range(numCourses):
+            dfs(i, set())
+            graph[i] = reqs.copy()
+            reqs.clear()
+
+        print(graph)
+
+        # implement search
+        for (prereq, course) in queries:
+            if not prereq in graph[course]:
+                res.append(False)
+            else:
+                res.append(True)
+
+        return res
+            
