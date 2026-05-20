b5c1a8838cb049f5680105af1fb2ff1974c60d22
Rishab Sanyal <rishabh106@gmail.com>
2026-05-20 14:29:15 -0700

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,39 @@
+"""
+At every level, we have 2 options
+- We pick one
+- We don't
+
+base case: when there's no more list left, return the list
+
+We have a universal list
+function(idx, curr_list):
+    if idx >= len(inp_list):
+        return
+
+    global_list.append(curr_list)
+
+    function(idx+1, curr_list+list[idx])
+    function(idx+1, list[idx])
+
+    
+
+
+    
+"""
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        self.res = []
+        
+        def track(idx, curr_list):
+            if idx >= len(nums):
+                self.res.append(curr_list)
+                return
+
+            track(idx+1, curr_list)
+            track(idx+1, curr_list+[nums[idx]])
+
+        track(0, [])
+
+        return self.res
