031b29248d20e3d66df5ebd7ada4fae2de77c4c9
Rishab Sanyal <rishabh106@gmail.com>
2026-05-20 12:11:37 -0700

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.py
@@ -0,0 +1,33 @@
+"""
+We could totally use a MIN heap for this
+
+Iterate through the list, check abs value and push on heap - nlg(n) time because of heapifying
+
+We could sort and go back and forth from the middle?
+If we have a sorted list, we could drill down on the closest window
+We could also sort it using a custom function and pick the first few elements, right?
+
+We have l and r from a sorted list
+We could iterate through the list and check the sum of difference between the first and last element of the window
+
+
+"""
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, k
+        start = 0
+        min_diff = abs(arr[0] - x) + abs(arr[k-1] - x)
+
+        for i in range(0, len(arr)-k+1):
+            new_diff = abs(arr[i] - x) + abs(arr[i+k-1] - x)
+
+            # print(arr[i], arr[i+k-1])
+            # print(new_diff)
+
+            if new_diff < min_diff:
+                min_diff = new_diff
+                l, r = i, len(arr)-i
+        
+        return arr[l:r]
+            
