51dc80ede5b7dd2d6644996191aa26d0fcfdcebd
Rishab Sanyal <rishabh106@gmail.com>
2026-05-20 10:36:43 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,37 @@
+"""
+Iterate with a set of characters and len(s1) through s2
+if the set matches, we compare and find if true
+
+if the set is equal, we compare dicts. Can we compare dicts directly in python? - YES
+"""
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+
+
+        s1_dict = Counter(s1)
+
+        if len(s1) == len(s2):
+            return s1_dict == Counter(s2)
+        
+        s2_dict = Counter()
+        num_pop = 0
+
+        for i in range(0, len(s2)):
+            if i >= len(s1):
+                s2_dict[s2[num_pop]] -= 1
+
+                if s2_dict[s2[num_pop]] <= 0:
+                    s2_dict.pop(s2[num_pop])
+
+                num_pop += 1
+                
+            s2_dict[s2[i]] += 1
+
+            if s2_dict == s1_dict:
+                return True
+
+        return s2_dict == s1_dict
