451c1d783d31acc502c93500b5855072c971f454
Rishab Sanyal <rishabh106@gmail.com>
2026-05-20 16:39:34 -0700

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -0,0 +1,42 @@
+"""
+We'll iterate through the graph
+we have the word, we make a list of it
+
+we go through the graph and if the word list is ever empty, we return False
+
+For each cell, we have 4 options until the length of the word -> O(m*4^n)
+"""
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        word_list = [i for i in word]
+
+        neighbors = [
+            [-1, 0],
+            [0, -1],
+            [1, 0],
+            [0, 1]
+        ]
+
+        def r(x, y, word_list):
+            if not word_list:
+                return True
+
+            if not (0 <= x < len(board)) or not (0 <= y < len(board[0])) or not (board[x][y] == word_list[0]):
+                return False
+
+            for (nx, ny) in neighbors:
+                ans = r(x+nx, y+ny, word_list[1:])
+                if ans:
+                    return True
+
+            return False
+            
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                res = r(i, j, word_list)
+                if res:
+                    return True
+
+        return False
