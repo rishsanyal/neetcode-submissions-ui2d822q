356cd71f03b47db7b9c8a9b73ee49c0c6286ae39
Rishab Sanyal <rishabh106@gmail.com>
2026-05-20 11:40:52 -0700

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.py
@@ -0,0 +1,29 @@
+"""
+we have two pointers l and r
+we keep a counter
+as we go on we keep adding the r value to the counter
+if the ctr value goes over target, we increase l and remove that from the ctr
+
+for eahc iteration, we track the length
+"""
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        start = end = 0
+        curr_sum = 0
+        res = float('inf')
+        res_set = False
+
+        while end < len(nums):
+            curr_sum += nums[end]
+
+            while curr_sum >= target:
+                res_set = True
+                res = min(res, end-start+1)
+                curr_sum -= nums[start]
+                start += 1
+
+
+            end += 1
+
+        return res if res_set else 0
