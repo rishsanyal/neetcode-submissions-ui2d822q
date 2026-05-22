89d61e6e5f344df25a1dc2ddd78d789353c1b216
Rishab Sanyal <rishabh106@gmail.com>
2026-05-22 13:45:46 -0700

    Add: unique-paths-ii - submission-5

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-5.py
@@ -0,0 +1,26 @@
+"""
+Robot goes from top-left to bottom right
+Can only move down or right 
+1's are obstacles
+"""
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        cache = {
+            (len(obstacleGrid)-1, len(obstacleGrid[0])-1): 1
+        }
+        
+        def r(x,y):
+            if not (0 <= x < len(obstacleGrid)) or not (0 <= y < len(obstacleGrid[0])) or obstacleGrid[x][y]:
+                return 0
+
+            if (x,y) in cache:
+                return cache[(x, y)]
+
+            cache[(x, y)] = r(x+1, y) + r(x, y+1)
+
+            return cache[(x, y)]
+
+        return r(0, 0)
+
+            
