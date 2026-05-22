ee0c9cbf43ff3b012525e40512713170318f0682
Rishab Sanyal <rishabh106@gmail.com>
2026-05-22 11:37:21 -0700

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.py
@@ -0,0 +1,40 @@
+"""
+For each number, we either add it or sutract it
+Not sure how DP comes into play here yet
+
+for each index, we either add or subtract
+
+we could have a cache and then as we go on, we memoize it
+
+[2, 2, 2] -> curr_sum, idx
+
+curr_sum + 2, idx
+curr_sum - 2, idx
+
+return 0 if out of index and target != curr_sum
+return 1 if out of idx and target == curr_sum
+"""
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        cache = {}
+
+        def r(idx, curr_sum):
+            
+            if idx == len(nums):
+                return (curr_sum == target)
+
+            if idx in cache:
+                return cache[idx]
+
+            cache[idx] = r(idx+1, curr_sum+nums[idx]) + r(idx+1, curr_sum-nums[idx])
+
+            return cache[idx]
+
+
+        r(0, 0)
+
+        return cache[0]
+
+
+        
