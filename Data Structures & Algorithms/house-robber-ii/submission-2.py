7e5289751d618ccfa1130a67366e561661086786
Rishab Sanyal <rishabh106@gmail.com>
2026-05-22 14:55:11 -0700

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,26 @@
+# Flag is False for last element
+# but can be true IN some conditions
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        cache = {}
+        
+        def dfs(i, limit):
+            if i >= limit:
+                return 0
+
+            if (i, limit) in cache:
+                return cache[(i, limit)]
+
+            cache[(i, limit)] = max(
+                dfs(i+1, limit),
+                nums[i] + dfs(i+2, limit)
+            )
+
+            return cache[(i, limit)]
+
+        return max(dfs(0, len(nums)-1), dfs(1, len(nums)))
