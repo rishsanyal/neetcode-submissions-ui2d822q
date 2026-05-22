bd862e584f8d1569ce4aedab5700f1906d5a7134
Rishab Sanyal <rishabh106@gmail.com>
2026-05-21 22:10:35 -0700

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        cache = {}
+        cache[0] = 0
+        cache[1] = 1
+
+        def r(num):
+            if num in cache:
+                return cache[num]
+
+            if num <= 0:
+                return 0
+
+            res = r(num-3) + r(num-2) + r(num-1)
+
+            cache[num] = res
+
+            return res
+
+        return r(n)
+
