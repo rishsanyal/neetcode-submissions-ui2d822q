bba4dc0b0f0260c7482dec88fabf04f55e39f562
Rishab Sanyal <rishabh106@gmail.com>
2026-05-26 11:40:51 -0700

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,31 @@
+"""
+on each level, we decrease the amount by the coin
+cache -- amount, prev_coin - ?
+
+(12, 0) = min((11, 1), (7, 1), (2, 1))
+
+"""
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        cache = defaultdict(lambda: float('inf'))
+        
+        def r(curr_amt, num_coins):
+            if curr_amt <= 0:
+                if curr_amt==0:
+                    cache[curr_amt] = num_coins
+                    return cache[curr_amt]
+
+                return float('inf')
+
+            for c in coins:
+                cache[curr_amt] = min(r(curr_amt-c, num_coins+1), cache[curr_amt])
+
+            return cache[curr_amt]
+
+        res = r(amount, 0)
+
+        return res if res != float('inf') else -1
+                
+
