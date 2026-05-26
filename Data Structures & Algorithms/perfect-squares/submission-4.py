d49d18bd26908b59302d9e7be57bace91af060cc
Rishab Sanyal <rishabh106@gmail.com>
2026-05-26 16:17:18 -0700

    Add: perfect-squares - submission-4

---
+++ Data Structures & Algorithms/perfect-squares/submission-4.py
@@ -0,0 +1,31 @@
+"""
+At every stage, we have to subtract the biggest square number down to 1
+int(math.sqrt(13)) = 3.6 ~ 3,2,1
+and keep going on that until we hit target
+
+We need to go in reverse order because we want the least amount of numbers
+"""
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+
+        max_num = int(math.sqrt(n))
+        cache = defaultdict(lambda: float('inf'))
+
+        def r(curr_num_count, curr_sum=0):
+            if curr_sum > n:
+                return n + 1
+
+            if curr_sum == n:
+                return curr_num_count
+
+            if curr_sum in cache:
+                return cache[curr_sum]
+
+            for i in range(max_num, 0, -1):
+                cache[curr_sum] = min(cache[curr_sum], r(curr_num_count+1, curr_sum+(i**2)))
+
+            return cache[curr_sum]
+
+
+        return r(0, 0)
