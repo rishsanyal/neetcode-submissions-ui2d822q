d19b1d83e12b2971f0deef02bca87fd28fc662f9
Rishab Sanyal <rishabh106@gmail.com>
2026-05-26 18:09:21 -0700

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.py
@@ -0,0 +1,37 @@
+"""
+At each level, we either include the next number or start from scratch
+
+[1,2,-3,4]
+
+[5, 2, -1, 10, -3, 4]
+
+we maintain a min, max for recursion
+"""
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+
+        c_max, c_min = 1, 1
+
+        ans = float('-inf')
+
+        for i in nums:
+            temp = c_max * i
+
+            c_max = max(
+                i*c_max,
+                i*c_min,
+                i
+            )
+
+            c_min = min(
+                temp,
+                i*c_min,
+                i
+            )
+
+            ans = max(c_max, ans)
+
+
+        return ans
+        
