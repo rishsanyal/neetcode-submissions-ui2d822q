bfa26d4456cf430255b4340057c7685cb6b011bf
Rishab Sanyal <rishabh106@gmail.com>
2026-06-10 15:15:43 -0700

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.py
@@ -0,0 +1,54 @@
+"""
+We could add the interval
+sort the array
+merge intervals as we go on?
+
+
+we could insert interval according to start time
+iterate forward and merge intervals 2 at a time
+
+[[1,3],[4,6]]
+[[1,2],[3,5],[4,6]]
+
+[[1,2],[3,6]]
+
+optimization - O(1) space
+
+"""
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for (idx, (start, end)) in enumerate(intervals):
+            if end > newInterval[1]:
+                intervals.insert(idx, newInterval)
+                newInterval = None
+                break
+
+            if newInterval[0] < start:
+                intervals.insert(idx-1, newInterval)
+                newInterval = None
+                break
+
+
+        if newInterval:
+            intervals.append(newInterval)
+
+        print(intervals)
+
+        for idx in range(len(intervals)):
+            curr_start, curr_end = intervals[idx]
+
+            if not res:
+                res.append([curr_start, curr_end])
+            else:
+                prev_start, prev_end = res[-1]
+
+                if (prev_start <= curr_start <= prev_end) or (prev_start <= curr_end <= prev_end) or (curr_start <= prev_start <= curr_end) or (curr_start <= prev_end <= curr_end):
+                    res[-1] = [min(prev_start, curr_start), max(curr_end, prev_end)]
+                else:
+                    res.append([curr_start, curr_end])
+
+        return res
