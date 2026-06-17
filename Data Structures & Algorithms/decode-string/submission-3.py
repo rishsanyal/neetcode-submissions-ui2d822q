a28700270a7a93ecf2fa56054d04d9c6ba1e27be
Rishab Sanyal <rishabh106@gmail.com>
2026-06-17 12:20:41 -0700

    Add: decode-string - submission-3

---
+++ Data Structures & Algorithms/decode-string/submission-3.py
@@ -0,0 +1,84 @@
+"""
+Stack of stacks?
+we have an int
+
+Every time we see a new number, we start a stack?
+
+abbbabbbc
+
+every time we see
+- bracket close, we pop from stack
+- brack open, we add to stack
+- if we pop from stack and the previous element is a str, we add to it
+
+abbbabbb c
+
+return .join
+
+
+axb3[z]4[c]
+
+axb, 3, z, 4, c
+"""
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+
+        curr_str = ''
+        tracking_num = False
+        stack = []
+
+        new_entry = True
+
+        for c in s:
+            if c.isnumeric():
+                # Was tracking a number previously
+                if new_entry:
+                    stack.append(c)
+                else:
+                    if stack and stack[-1].isnumeric():
+                        stack[-1] += c
+                    else:
+                        stack.append(c)
+
+                new_entry = False
+    
+            elif c == "[":
+                new_entry = True
+            elif c == "]":
+                # pop 2 elements
+                # make a string
+                # then keep popping
+                # if str: add to str
+                # if num: break
+
+                print(stack)
+
+                top_str, top_num = stack.pop(), stack.pop()
+                top_str = top_str * int(top_num)
+
+                if stack and stack[-1].isalpha():
+                    stack[-1] += top_str
+                else:
+                    stack.append(top_str)
+
+                print(stack)
+
+                new_entry = True
+            else:
+                if stack and stack[-1].isalpha():
+                    stack[-1] += c
+                else:
+                    stack.append(c)
+
+                tracking_num = False
+
+        if curr_str:
+            stack.append(curr_str)
+            curr_str = ''
+
+        return("".join(stack))
+
+            
+
+        
