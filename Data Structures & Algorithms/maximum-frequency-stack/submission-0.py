0b9e1f7352f4f4af7e4ec9afd4c66e5b09fa2a1c
Rishab Sanyal <rishabh106@gmail.com>
2026-06-18 18:05:16 -0700

    Add: maximum-frequency-stack - submission-0

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-0.py
@@ -0,0 +1,35 @@
+"""
+- We need a counter dict to count the frequency
+- We need a list with frequency as it's index
+
+We use the list and add numbers to the index according to it's frequency
+"""
+
+
+
+class FreqStack:
+
+    def __init__(self):
+        self.tracker = defaultdict(int)
+        self.stacks = []
+        
+    def push(self, val: int) -> None:
+        self.tracker[val] += 1
+
+        while len(self.stacks) <= self.tracker[val]:
+            self.stacks.append([])
+
+        self.stacks[self.tracker[val]].append(val)
+
+    def pop(self) -> int:
+        while self.stacks and not self.stacks[-1]:
+            self.stacks.pop()
+
+        return self.stacks[-1].pop()
+        
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
