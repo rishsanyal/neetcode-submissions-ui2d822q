3ab67e737a089224662084418047972eef154a66
Rishab Sanyal <rishabh106@gmail.com>
2026-06-18 17:42:57 -0700

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,76 @@
+"""
+
+
+
+Let's have 2 dictionaries
+t_dict - counter
+s_dict - DefaultDict
+
+we have l, r, 0, 0
+
+when the number of keys is the same, we iterate through them
+if the count is different on any one, we continue
+
+if the count on all is the same, we increase l untilwe hit a character in counter_t
+
+go from there - r += 1
+"""
+
+"""
+s="ODEBANC"
+t="ABC"
+
+t_dict = {A:1, B:1, C:1}
+
+l, r = 0, 0
+
+BEC
+l, r = 0, 5
+
+"""
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        if len(t) > len(s):
+            return ''
+
+        res = ''
+        res_len = len(s)+len(t)
+
+        t_dict = Counter(t)
+        s_dict = defaultdict(int)
+
+        l, r = 0, 0
+
+        while r < len(s):
+            curr_char = s[r]
+
+            if curr_char in t_dict:
+                s_dict[curr_char] += 1
+
+                if len(t_dict.keys()) == len(s_dict.keys()):
+                    for key, val in t_dict.items():
+                        if val != s_dict[key]:
+                            break
+
+                    while s_dict.keys() == t_dict.keys() and l <= r:
+                        # print(s[l:r+1], l, r )
+                        
+                        if res_len >= r-l+1:
+                            res_len = r-l+1
+                            res = s[l:r+1]
+
+                        if s[l] in t_dict and l < r:
+                            s_dict[s[l]] -= 1
+
+                            if s_dict[s[l]] <= 0:
+                                s_dict.pop(s[l])
+
+                        l += 1
+
+            r += 1
+
+        return res
+        
