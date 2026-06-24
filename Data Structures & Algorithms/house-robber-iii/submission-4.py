1fb7d16eac269469639ff02ddccdfe92b49572f8
Rishab Sanyal <rishabh106@gmail.com>
2026-06-24 12:31:07 -0700

    Add: house-robber-iii - submission-4

---
+++ Data Structures & Algorithms/house-robber-iii/submission-4.py
@@ -0,0 +1,67 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+At each level we have these options
+- We pick current node (if we can)
+- We skip current node (in both cases)
+
+We check the max loot returned at each level and return that
+
+at each level, we return max possible loot. we can get
+
+def traverse(node, can_select=True):
+    if not node:
+        return 0
+
+    if can_select:
+        options = max(
+            node.val + traverse(node.left, False),
+            node.val + traverse(node.right, False),
+            traverse(node.right, True),
+            traverse(node.left, True)
+        )
+    else:
+        options = max(
+            traverse(node.right, True),
+            traverse(node.left, True)
+        )
+
+    return options
+"""
+
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+
+        cache = {}
+        res = 0
+        
+        def traverse(node, can_select=True):
+            nonlocal res
+
+            if not node:
+                return 0
+
+            if (node, can_select) in cache:
+                return cache[(node, can_select)]
+
+            if can_select:
+                options = max(
+                    node.val + traverse(node.left, False) + traverse(node.right, False),
+                    traverse(node.left, True) + traverse(node.right, True)
+                )
+            else:
+                options = traverse(node.left, True) + traverse(node.right, True)
+
+            cache[(node, can_select)] = options
+            return options
+
+        traverse(root)
+
+        return cache[(root, True)]
+
+        
