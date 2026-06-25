47b7ef0da10760de2d2e0ca5631c1cae928d9399
Rishab Sanyal <rishabh106@gmail.com>
2026-06-24 21:16:36 -0700

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.py
@@ -0,0 +1,42 @@
+"""
+
+we need to check if [current index: current index + len(word)] is in the word dict
+if so, we add to a list
+
+if not, we continue to the next word
+
+
+base case: When the index == len(s) we add to a global result list
+
+def dfs(curr_idx, inp_list=[]):
+    if curr_idx == len(s):
+        result.append(inp_list[:]) # because we need to make a copy
+
+    if curr_idx > len(s):
+        return
+
+    for word in wordDict:
+        if s[curr_idx: curr_idx + len(word)] in wordDict:
+            dfs(curr_idx + len(word) - 1, inp_list + [word])
+"""
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        result = []
+        wordSet = set(wordDict)
+
+        def dfs(curr_idx, inp_list=[]):
+            if curr_idx == len(s):
+                result.append(' '.join(inp_list[:]))
+
+            if curr_idx > len(s):
+                return
+
+            for word in wordSet:
+                if s[curr_idx: curr_idx + len(word)] == word and s[curr_idx: curr_idx + len(word)] in wordSet:
+                    # print(s[curr_idx: curr_idx + len(word)], word)
+                    dfs(curr_idx + len(word), inp_list + [word])
+
+        dfs(0)
+        
+        return result
