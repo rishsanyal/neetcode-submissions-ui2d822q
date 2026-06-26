8b5076a43c458eeae3ae321b7e88bb50674dfa7c
Rishab Sanyal <rishabh106@gmail.com>
2026-06-26 00:20:53 -0700

    Add: stone-game-iii - submission-3

---
+++ Data Structures & Algorithms/stone-game-iii/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        dp = {}
+
+
+        def dfs(i, turn):
+            if i >= n:
+                return 0
+
+            if (i, turn) in dp:
+                return dp[(i, turn)]
+
+            res = float("-inf") if turn else float("inf")
+            score: int = 0
+
+            for j in range(i, min(i+3, n)):
+                if turn:
+                    score += stoneValue[j]
+                    res = max(res, score + dfs(j+1, False))
+                else:
+                    score -= stoneValue[j]
+                    res = min(res, score + dfs(j+1, True))
+
+            dp[(i, turn)] = res
+
+            return res
+
+        result = dfs(0, True)
+
+        if result == 0:
+            return "Tie"
+
+        return "Alice" if result > 0 else "Bob"
