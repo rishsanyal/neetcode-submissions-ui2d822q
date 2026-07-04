f9731f00748ad34712763e60643e65e3d53bce4e
Rishab Sanyal <rishabh106@gmail.com>
2026-07-03 21:33:03 -0700

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.py
@@ -0,0 +1,54 @@
+"""
+O(1) space is fine because the moment we find one we update the entire matrix the moment we see a 0
+we also track the last visited row and col and visit after that
+
+for r in row
+    for c in col
+
+        if col < last_visited:
+            don't go
+
+        if 0:
+            set all of it
+            set last visited col
+            move on from col
+
+
+
+"""
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        
+        last_visited_col = -1
+        last_visited_row = -1
+
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if col <= last_visited_col and row <= last_visited_row:
+                    continue
+
+                if matrix[row][col] == 0:
+                    # set row, col to 0
+
+                    for zero_row in range(len(matrix)):
+                        matrix[zero_row][col] = 'EMPTY'
+                        print(zero_row, col)
+
+                    for zero_col in range(len(matrix[0])):
+                        matrix[row][zero_col] = 'EMPTY'
+                        print(row, zero_col)
+
+                    last_visited_col = col
+                    last_visited_row = row
+                    continue
+
+            
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == 'EMPTY':
+                    matrix[row][col] = 0
+        
+
+        return
