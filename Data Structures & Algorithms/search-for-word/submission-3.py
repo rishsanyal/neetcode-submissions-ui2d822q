f49a884c1c32a844b1cb012717722ccdef6f7812
Rishab Sanyal <rishabh106@gmail.com>
2026-08-03 17:27:20 -0700

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        neighbors = [
+            (-1, 0),
+            (0, -1),
+            (1, 0),
+            (0, 1)
+        ]
+
+        w = list(word)
+
+        def check(x, y, word_list):
+
+            if not word_list:
+                return True
+
+            if (not (0 <= x < len(board))) or (not (0 <= y < len(board[0]))):
+                return False
+
+            if board[x][y] != word_list[0]:
+                return False
+
+            for (dx, dy) in neighbors:
+                if check(x+dx, y+dy, word_list[1:]):
+                    return True
+
+            return False
+
+
+        for x in range(len(board)):
+            for y in range(len(board[0])):
+                if check(x, y, w):
+                    return True
+
+
+        return False
