662acb2762bf771713cd76fd4fb3c8c66900e258
Rishab Sanyal <rishabh106@gmail.com>
2026-08-11 15:55:45 -0700

    Add: palindrome-partitioning - submission-3

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-3.py
@@ -0,0 +1,38 @@
+"""
+
+We track idx, curr word, curr palindromes
+
+if curr_word+s[idx] is a palindrome
+    r(idx+1, '', curr_palindromes+[curr_word+s[idx]])
+
+r(idx+1, curr_word+s[idx], curr_palindromes)
+
+"""
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+
+        res = []
+
+        def r(idx, curr_word, curr_palindromes):
+            if idx == len(s):
+                if not curr_word:
+                    res.append(curr_palindromes)
+
+                return
+
+
+            new_word = curr_word + s[idx]
+
+            if new_word == new_word[::-1]:
+                r(idx+1, '', curr_palindromes+[new_word])
+
+            r(idx+1, new_word, curr_palindromes)
+
+            return
+
+        r(0, '', [])
+
+        return res
+        
