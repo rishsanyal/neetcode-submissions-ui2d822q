ad068841ecb985fb84ca1b935d4e2d50aeae2fe3
Rishab Sanyal <rishabh106@gmail.com>
2026-08-12 10:27:42 -0700

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.py
@@ -0,0 +1,50 @@
+"""
+
+2 pointer solution for swapping numbers
+
+[1,2,3,4,5,6,7,8]
+[5,6,7,8,1,2,3,4]
+
+- reverse all
+- reverse from 0 to k and so on ...
+
+[1,2,3,4,5,6]
+[3,4,5,6,1,2]
+
+[1,2,3,4,5,6,7]
+[7,6,5,4,3,2,1]
+"""
+
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        nums.reverse()
+
+        if k == 0:
+            return
+
+        l = 0
+        r = k-1
+
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+
+            l += 1
+            r -= 1
+
+        l = k
+        r = len(nums) - 1
+
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+
+            l += 1
+            r -= 1
+
+        
+        
