aa41c7dbdfd5dfd38ed3974f017e20f8da1edc47
Rishab Sanyal <rishabh106@gmail.com>
2026-08-12 21:51:22 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        tracker = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        stack = []
+
+        for bracket in s:
+            if bracket not in tracker:
+                stack.append(bracket)
+            else:
+                prev_bracket = stack.pop()
+
+                if prev_bracket != tracker[bracket]:
+                    return False
+
+        return stack == []
