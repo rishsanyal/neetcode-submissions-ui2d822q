e23369e568a36b9ff7a7ac4d84823fce8a9fbe34
Rishab Sanyal <rishabh106@gmail.com>
2026-08-12 21:54:03 -0700

    Update: implement-queue-using-stacks - submission-3

--- Data Structures & Algorithms/implement-queue-using-stacks/submission-3.py
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-3.py
@@ -1,48 +1,23 @@
-class Stack:
-    def __init__(self):
-        self.stack = []
-
-    def push(self, val):
-        self.stack.append(val)
-
-    def pop(self):
-        return self.stack.pop(-1)
-
-    def __bool__(self):
-        return bool(self.stack)
-
-    def __repr__(self):
-        return str(self.stack)
-
-
 class MyQueue:
 
     def __init__(self):
-        self.s1, self.s2 = Stack(), Stack()
+        self.s1 = []
+        self.s2 = []
 
     def push(self, x: int) -> None:
-        while self.s1:
-            self.s2.push(self.s1.pop())
-        self.s2.push(x)
-
-        while self.s2:
-            self.s1.push(self.s2.pop())
-
-        print(self.s1)
+        self.s1.append(x)
 
     def pop(self) -> int:
-        return self.s1.pop()
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
 
     def peek(self) -> int:
-        return self.s1.stack[-1]
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
 
     def empty(self) -> bool:
-        return bool(self.s1.stack == [])
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+        return max(len(self.s1), len(self.s2)) == 0
