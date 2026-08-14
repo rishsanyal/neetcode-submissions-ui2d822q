23e181235599d7446b7a960657cdbf6862de2e12
Rishab Sanyal <rishabh106@gmail.com>
2026-08-14 13:49:59 -0700

    Add: asteroid-collision - submission-10

---
+++ Data Structures & Algorithms/asteroid-collision/submission-10.py
@@ -0,0 +1,52 @@
+
+"""
++ve means right, -ve means left
+abs value is it's size
+
+smaller one explodes
+if equal both explode
+
+stack for this
+"""
+
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        
+        stack = []
+
+        for asteroid in asteroids:
+
+            # print(stack)
+
+            previous_asteroid = stack[-1] if stack else None
+
+            if previous_asteroid is None:
+                stack.append(asteroid)
+                continue
+            
+            prev_asteroid_side = True if previous_asteroid >= 0 else False
+            curr_asteroid_side = True if asteroid >= 0 else False
+
+            previous_asteroid_size = abs(previous_asteroid)
+            curr_asteroid_size = abs(asteroid)
+
+            if prev_asteroid_side == curr_asteroid_side:
+                # both +ve or both -ve
+                stack.append(asteroid)
+            elif prev_asteroid_side and not curr_asteroid_side:
+                # prev asteroid +ve, curr -ve
+                if previous_asteroid_size > curr_asteroid_size:
+                    continue
+                elif previous_asteroid_size < curr_asteroid_size:
+                    stack.pop()
+                    stack.append(asteroid)
+                else:
+                    stack.pop()
+            else:
+                # prev -ve curr positive
+                stack.append(asteroid)
+
+        return stack
+
