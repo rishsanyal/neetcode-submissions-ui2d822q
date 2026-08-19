72b8c11b86fa67c12cd6293cc27aa9d3a75bd49d
Rishab Sanyal <rishabh106@gmail.com>
2026-08-19 15:35:22 -0700

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.py
@@ -0,0 +1,40 @@
+"""
+we need to go inside out from each character and keep updating the res
+
+O(N*2) worst case
+
+  a b a b d
+a 1 0 1 0 0
+b 0 1 0 1 0
+a 1 0 1 0 0
+b 0 1 0 1 0
+d 0 0 0 0 1
+
+
+
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        res = ""
+        res_len = 0
+
+        for i in range(len(s)):
+            for j in range(2):
+                l,r = i, i+j
+
+                while (0 <= l < len(s)) and (0 <= r < len(s)):
+                    if s[l] == s[r]:
+                        if len(s[l:r+1]) > res_len:
+                            res_len = r-l+1
+                            res = s[l:r+1]
+
+                        l -= 1
+                        r += 1
+                    else:
+                        break
+
+        return res
+        
