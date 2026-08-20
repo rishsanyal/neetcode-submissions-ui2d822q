f506f38e9d10ab48627ea9c5c18d919b80c7e66a
Rishab Sanyal <rishabh106@gmail.com>
2026-08-20 11:21:03 -0700

    Add: coin-change-ii - submission-6

---
+++ Data Structures & Algorithms/coin-change-ii/submission-6.py
@@ -0,0 +1,47 @@
+"""
+
+We're given a set of coins
+we have to get the total combinations to get an amount
+
+we have to use backtracking since we have O(N) choices at every level
+
+we can't go over the total amount
+
+we track the current amount
+for every option we add the current amount and go on
+if we exceed the amount, we return
+
+at each point we either skip a coin or pick it
+"""
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        ans = 0
+        cache = {}
+
+        def r(curr_idx, curr_sum):
+
+            if (curr_idx, curr_sum) in cache:
+                return cache[(curr_idx, curr_sum)]
+
+            if curr_idx >= len(coins):
+                return 0
+
+            if curr_sum > amount:
+                return 0
+
+            if curr_sum == amount:
+                return 1
+
+            res = 0
+
+            for i in range(curr_idx, len(coins)):
+                res += r(i, curr_sum+coins[i])
+            
+            cache[(curr_idx, curr_sum)] = res
+
+            return res
+
+        return r(0, 0)
+
+                
