c5300570fbd123f9de3001d8a8dd550d10233ef9
Rishab Sanyal <rishabh106@gmail.com>
2026-08-20 09:15:04 -0700

    Add: integer-break - submission-7

---
+++ Data Structures & Algorithms/integer-break/submission-7.py
@@ -0,0 +1,34 @@
+"""
+
+global res
+
+- i from n -> 0
+ if n%i == 0:
+    res = max(res, (i)**(n/i))
+
+"""
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        ans = 0
+        cache = {}
+
+        def r(num):
+            nonlocal ans
+            res = 1
+
+            if num in cache:
+                return cache[num]
+            
+            if num == 1:
+                return 1
+
+            cache[num] = num
+
+            for i in range(1, num):
+                res = r(i) * r(num-i)
+                cache[num] = max(res, cache[num])
+
+            return cache[num]
+
+        return r(n)
