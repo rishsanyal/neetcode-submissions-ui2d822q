b038208c0d10436621cf9d00c511416c9d10a574
Rishab Sanyal <rishabh106@gmail.com>
2026-08-19 18:17:12 -0700

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -0,0 +1,66 @@
+"""
+
+12
+- 1 '2'
+    - 2 ''
+- 12 ''
+
+
+We track the idx of the string
+
+"""
+
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        char_tracker = {}
+
+        for i in range(65, 65+26):
+            char_tracker[str(i-65+1)] = chr(i)
+
+        cache = {}
+        res = 0
+
+        def __helper(idx):
+
+            res = 0
+
+            if idx in cache:
+                return cache[idx]
+            
+            if idx >= len(s):
+                return 1
+
+            if s[idx] not in char_tracker:
+                return 0
+
+            if idx <= len(s)-2:
+                one_char = s[idx]
+                two_char = s[idx:idx+2]
+
+                if one_char in char_tracker:
+                    res += __helper(idx+1)
+
+                if two_char in char_tracker:
+                    res += __helper(idx+2)
+
+            elif idx <= len(s)-1:
+                one_char = s[idx]
+
+                if one_char in char_tracker:
+                    res += __helper(idx+1)
+
+            cache[idx] = res
+
+            return cache[idx]
+
+        ans = __helper(0)
+
+        return ans
+
+
+        
+
+        
