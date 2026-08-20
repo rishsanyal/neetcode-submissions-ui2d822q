2a7154275f0e51c9301d6fc4a3d86ab54e3dfb63
Rishab Sanyal <rishabh106@gmail.com>
2026-08-20 14:59:40 -0700

    Add: buy-and-sell-crypto-with-cooldown - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-9.py
@@ -0,0 +1,39 @@
+"""
+At each point we have 2 options
+Hold
+Buy/Sell
+"""
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cache = {}
+
+        def r(idx, buy=True):
+            if idx >= len(prices):
+                return 0
+
+            if (idx, buy) in cache:
+                return cache[(idx, buy)]
+
+            res = r(idx+1, buy)
+
+            if buy:
+                res = max(
+                    res, 
+                    r(idx+1, not buy) - prices[idx]
+                )
+    
+            else:
+                res = max(
+                    res,
+                    r(idx+2, not buy) + prices[idx]
+                )
+
+            cache[(idx, buy)] = res
+
+            return res
+
+        return r(0)
+
+        
