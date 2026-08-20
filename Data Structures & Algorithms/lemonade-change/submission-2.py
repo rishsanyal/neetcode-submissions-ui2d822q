26f80d632db08099f081da7ff141cbcf7c657d38
Rishab Sanyal <rishabh106@gmail.com>
2026-08-20 16:00:56 -0700

    Add: lemonade-change - submission-2

---
+++ Data Structures & Algorithms/lemonade-change/submission-2.py
@@ -0,0 +1,78 @@
+"""
+
+- We track the number of bills we have in a dict
+- We could be greedy and return the big bills
+
+{5: 0, 10: 0, 20: 0}
+
+for bill in bills:
+
+    change = bill - 5
+
+    # 5
+    if change == 0:
+        tracker[bill] += 1
+        continue
+
+    # 10
+    if change == 5:
+        if tracker[5] >= 1:
+            tracker[bill] += 1
+            continue
+        else:
+            return False
+
+    # 20
+    if change == 15:
+        if tracker[5] >= 1 and tracker[10] >= 1:
+            tracker[bill] += 1
+            continue
+        else:
+            return False
+
+return True
+
+            
+
+"""
+
+
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        
+        tracker = {5: 0, 10: 0, 20: 0}
+
+        for bill in bills:
+
+            change = bill - 5
+
+            # 5
+            if change == 0:
+                tracker[5] += 1
+                continue
+
+            # 10
+            if change == 5:
+                if tracker[5] >= 1:
+                    tracker[bill] += 1
+                    tracker[5] -= 1
+                    continue
+                else:
+                    return False
+
+            # 20
+            if change == 15:
+                if tracker[5] >= 1 and tracker[10] >= 1:
+                    tracker[bill] += 1
+                    tracker[5] -= 1
+                    tracker[10] -= 1
+                    continue
+                elif tracker[5] >= 3:
+                    tracker[bill] += 1
+                    tracker[5] -= 3
+                    continue
+                else:
+                    return False
+
+        return True
