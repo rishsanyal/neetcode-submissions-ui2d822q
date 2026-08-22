40c63232bedf9903a31286e151421f65c9f9e298
Rishab Sanyal <rishabh106@gmail.com>
2026-08-22 15:04:21 -0700

    Add: buy-and-sell-crypto-with-cooldown - submission-10

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-10.py
@@ -0,0 +1,64 @@
+"""
+
+states
+- buy -> idx+1
+- sell -> idx+2
+
+- We need to track the max profit
+- We have to buy first
+- At each step, we could either Do nothing OR we buy/sell
+
+terminal case: We run out of stocks
+    we return what we're holding
+
+we add when we sell and - when we buy
+
+
+
+    def r(idx=0, state=True):
+        if idx >= len(prices):
+            return 0
+
+        if (idx, state) in cache:
+            return cache[(idx, state)]
+
+        res = r(idx+1, state)
+
+        if state:
+            res = max(res, prices[idx] + r(idx+1, not state))
+        else:
+            res = max(res, r(idx+1, not state) - prices[idx])
+
+        return res
+
+[2, 3, 4]
+
+[3,4] - 2
+
+    
+
+"""
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        cache = {}
+
+        def r(idx=0, state=True):
+            if idx >= len(prices):
+                return 0
+
+            if (idx, state) in cache:
+                return cache[(idx, state)]
+
+            res = r(idx+1, state)
+
+            if state:
+                res = max(res, r(idx+1, not state) - prices[idx])
+            else:
+                res = max(res, r(idx+2, not state) + prices[idx])
+
+            return res
+            
+        return r(0)
