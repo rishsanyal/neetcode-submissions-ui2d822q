d98af5ed1ecd8ca014547350bec6ff1733db1188
Rishab Sanyal <rishabh106@gmail.com>
2026-08-26 15:19:29 -0700

    Add: candy - submission-3

---
+++ Data Structures & Algorithms/candy/submission-3.py
@@ -0,0 +1,38 @@
+"""
+we can do this in 2 passes
+left-right -> 1, len(ratings)
+right-left -> (len(ratings)-2, 0)
+
+
+[1,1,1]
+
+[4,3,5]
+[1,1,2]
+[2,1,2] -> 5
+
+[1,1,1]
+[2,3,3]
+[1,2,1]
+[1,2,1] - 4
+
+float('inf') + [1,3,2,2,1] + float('inf')
+
+[inf,1,2,3,4,5,inf]
+[1,1,2,3,4,5,1]
+"""
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        res = [1] * len(ratings)
+
+        for i in range(len(ratings)):
+            if i > 0 and i < len(ratings)-1 and ratings[i-1] < ratings[i] > ratings[i+1]:
+                res[i] = 1 + max(res[i-1], res[i], res[i+1])
+            elif i > 0 and ratings[i-1] < ratings[i]:
+                res[i] = 1 + max(res[i-1], res[i])
+            elif i < len(ratings)-1 and ratings[i] > ratings[i+1]:
+                res[i] = 1 + max(res[i], res[i+1])
+
+        return sum(res)
+
+        
