415e7db2424990c4bb7174316c82d9e6e0b5f5c1
Rishab Sanyal <rishabh106@gmail.com>
2026-08-27 11:12:58 -0700

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.py
@@ -0,0 +1,38 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+"""
+
+"""
+What's a conflict? 
+It's a start time between the previous meeting
+
+- Sort by start time
+- validate against previous one
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+
+        if not intervals:
+            return True
+
+        curr_start, curr_end = intervals[0].start, intervals[0].end
+
+        for i in range(1, len(intervals)):
+            curr_interval = intervals[i]
+
+            if (curr_start <= curr_interval.start < curr_end):
+                return False
+
+            curr_start, curr_end = curr_interval.start, curr_interval.end
+
+        return True
+            
+
+            
