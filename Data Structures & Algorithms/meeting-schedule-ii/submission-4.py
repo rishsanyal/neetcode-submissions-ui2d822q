9a8cc1c66cf7073170a39367824356108ca00519
Rishab Sanyal <rishabh106@gmail.com>
2026-08-27 21:03:21 -0700

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,38 @@
+# """
+# Definition of Interval:
+# class Interval(object):
+#     def __init__(self, start, end):
+#         self.start = start
+#         self.end = end
+# """
+
+# """
+# - sort meetings by start time
+# - We check for a conflict
+#     conflict: start time between the previous meeting's time
+
+
+# - sort by end time?
+# [(0,40),(5,10),(15,20)]
+
+# [(5, 10), (15, 24), (16, 30), (25, 40)]
+# we could track the conflict and use the smaller time for the conference rooms?
+
+
+# [(5, 10), (15, 24), (16, 30), (25, 40)]
+
+# """
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x: x.start)
+
+        h = []
+
+        for interval in intervals:
+            if h and h[0] <= interval.start:
+                heapq.heappop(h)
+
+            heapq.heappush(h, interval.end)
+
+        return len(h)
