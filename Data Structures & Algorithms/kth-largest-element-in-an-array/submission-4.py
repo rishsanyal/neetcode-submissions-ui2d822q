f6ebdb8b405c70408d99cebe41498eb17e517ca7
Rishab Sanyal <rishabh106@gmail.com>
2026-09-03 13:50:59 -0700

    Add: kth-largest-element-in-an-array - submission-4

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-4.py
@@ -0,0 +1,23 @@
+"""
+We pop it all in a max heap
+pop k elements
+
+
+[2,3,1,5,4]
+[5,4,3,2,1] k = 2
+
+sort and len(nums)-kth index
+"""
+
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        h = []
+
+        nums.sort()
+
+        if len(nums)-k >= 0:
+            return nums[len(nums)-k]
+        
+        return -1
