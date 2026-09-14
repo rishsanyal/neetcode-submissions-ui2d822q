09670f7800d7a7228bf5d104b3fb0334d6fb862a
Rishab Sanyal <rishabh106@gmail.com>
2026-09-13 23:17:45 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,49 @@
+"""
+
+- We need a dependecy graph
+- we need to track visited classes and the longst path they can take
+- if at any point, the answer = n-1, we return
+
+- there should be no cycles - if cycle, set the answer to -1
+"""
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        cache = {}
+        graph = defaultdict(set)
+
+        state = False
+
+        for a,b in prerequisites:
+            graph[a].add(b)
+
+        def dfs(curr_course, visited):
+            if curr_course in visited:
+                return -1
+
+            if curr_course in cache:
+                return cache[curr_course]
+
+            if curr_course not in graph:
+                return 1
+
+            visited.add(curr_course)
+            res = 0
+
+            for dependecy in graph[curr_course]:
+                temp = dfs(dependecy, visited)
+
+                if temp == -1:
+                    res = -1
+                    break
+                
+                res += temp
+
+            cache[curr_course] = res
+            return res
+
+        ans = dfs(0, set())
+
+        print(ans)
+
+        return ans == numCourses-1
