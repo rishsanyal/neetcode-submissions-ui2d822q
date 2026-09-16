b4c27c8013d4942b65436db7888c3770cf3a09ec
Rishab Sanyal <rishabh106@gmail.com>
2026-09-16 11:53:43 -0700

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.curr_min = None
+
+    def push(self, val: int) -> None:
+        if self.curr_min is None:
+            self.curr_min = val
+
+        self.curr_min = min(self.curr_min, val)
+        self.stack.append((val, self.curr_min))
+        
+    def pop(self) -> None:
+        _, prev_min = self.stack.pop()
+
+        if self.stack:
+            self.curr_min = self.stack[-1][1]
+        else:
+            self.curr_min = None
+        
+    def top(self) -> int:
+        if not self.stack:
+            return -1
+
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.curr_min
+        # if self.stack:
+        #     return self.stack[-1][1]
+
+        # return -1
+        
