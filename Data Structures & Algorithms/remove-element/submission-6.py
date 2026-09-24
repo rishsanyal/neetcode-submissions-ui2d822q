e9b4f4b72ecf5f4566bd91476c5cab8154780a61
Rishab Sanyal <rishabh106@gmail.com>
2026-09-23 17:41:30 -0700

    Add: remove-element - submission-6

---
+++ Data Structures & Algorithms/remove-element/submission-6.py
@@ -0,0 +1,27 @@
+"""
+We maintain a pointer for the last index
+we start from the left
+l = 0
+r = len(nums) - 1
+while l < r:
+    if val == val:
+        we switch
+        we decrease r
+    else:
+        l += 1
+
+"""
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            if nums[l] == val:
+                nums[l], nums[r] = nums[r], nums[l]
+                r -= 1
+            else:
+                l += 1
+
+        return l
