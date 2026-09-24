1ac5162b234b6908e104714ad5c8e68d8108a21b
Rishab Sanyal <rishabh106@gmail.com>
2026-09-24 13:29:38 -0700

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,26 @@
+"""
+
+Brute Force:
+i in range(len(nums)):
+    sum = 0
+    j in range(i: len(nums))
+
+
+We create a dict as we go along
+we track the sum and the count of sumarrays with that sum in that map. 
+once we reach a sum, we subtract that from target and check if we have the difference in the map
+"""
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        tracker = {0:1}
+        curr_sum = 0
+        res = 0
+
+        for num in nums:
+            curr_sum += num
+            res += tracker.get(curr_sum-k, 0)
+            tracker[curr_sum] = tracker.get(curr_sum, 0) + 1
+
+        return res
